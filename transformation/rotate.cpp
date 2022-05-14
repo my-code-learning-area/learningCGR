0f82bf2668d053c8c3b041e7d9afe4c9dd0e3515
sumitkawale <sumitkawale2002@gmail.com>
2022-05-15 02:19:50 +0530

    saving; rotation done

--- transformation/rotate.cpp
+++ transformation/rotate.cpp
@@ -22,7 +22,7 @@ void rotate(int x, int y, double rotation, int mode) {
 
 void lineCircle(int x1, int y1, int x2, int y2) {
     glColor3d(0, 0, 1);
-    glLineWidth(5);
+    glLineWidth(12);
 
     glBegin(GL_LINES);
         glVertex2d(x1, y1);
@@ -43,15 +43,83 @@ void lineCircle(int x1, int y1, int x2, int y2) {
     } 
 }
 
+void rotateOnIT(int x1, int y1, int x2, int y2) {
+    glColor3d(0, 0, 1);
+    glLineWidth(1);
+
+    glBegin(GL_LINES);
+        glVertex2d(x1, y1);
+        glVertex2d(x2, y2);
+    glEnd();
+
+    double rotateBy = 0.02;
+
+    double xc = x1 + ((x2 - x1) / 2);
+    double yc = y1 + ((y2 - y1) / 2);
+
+    double xx1 = x1 - xc;
+    double yy1 = y1 - yc;
+    double xx2 = x2 - xc;
+    double yy2 = y2 - yc;
+
+    double xr1, yr1, xr2, yr2;
+
+    glBegin(GL_LINES);
+        glVertex2d(xx1 + xc, yy1 + yc);
+        glVertex2d(xx2 + xc, yy2 + yc);
+    glEnd();
+
+    // for(rotateBy; rotateBy < 36.26; rotateBy += 0.02) {
+    for(rotateBy; rotateBy < 36.26; rotateBy += 0.3) {
+        xr1 = (xx1 * cos(rotateBy)) + (yy1 * (-sin(rotateBy)));
+        yr1 = (xx1 * sin(rotateBy)) + (yy1 * (cos(rotateBy)));
+        xr2 = (xx2 * cos(rotateBy)) + (yy2 * (-sin(rotateBy)));
+        yr2 = (xx2 * sin(rotateBy)) + (yy2 * (cos(rotateBy)));
+        
+        // xr1 = (xx1 * cos(rotateBy)) + (yy1 * (-sin(rotateBy)));
+        // yr1 = (xx1 * sin(rotateBy)) + (yy1 * (cos(rotateBy)));
+        // xr2 = (xx2 * cos(rotateBy)) + (yy1 * (-sin(rotateBy)));
+        // yr2 = (xx1 * sin(rotateBy)) + (yy2 * (cos(rotateBy)));
+
+        // xr1 = (xx2 * cos(rotateBy)) + (yy2 * (-sin(rotateBy)));
+        // yr1 = (xx1 * sin(rotateBy)) + (yy1 * (cos(rotateBy)));
+        // xr2 = (xx2 * cos(rotateBy)) + (yy1 * (-sin(rotateBy)));
+        // yr2 = (xx1 * sin(rotateBy)) + (yy2 * (cos(rotateBy)));
+        
+        // xr1 = (xx1 * cos(rotateBy)) + (yy1 * (-sin(rotateBy)));
+        // yr1 = (xx1 * sin(rotateBy)) + (yy1 * (cos(rotateBy)));
+        // xr2 = (xx2 * cos(rotateBy)) + (yy1 * (-sin(rotateBy)));
+        // yr2 = (xx2 * sin(rotateBy)) + (yy2 * (cos(rotateBy)));
+
+
+        glBegin(GL_LINES);
+            glVertex2d(xr1 + xc, yr1 + yc);
+            glVertex2d(xr2 + xc, yr2 + yc);
+        glEnd();
+        glFlush();
+
+        // for(int i = 0; i < 500; i++)
+        //     for(int j = 0; j < 500; j++) {}
+    } 
+}
+
 void display() {
-    lineCircle(10, 13, 20, 20);
+    // lineCircle(10, 13, 20, 20);
+
+    // rotateOnIT(-250, 0, 250, 0);
+    rotateOnIT(50, 50, 250, 250);
+    rotateOnIT(50, -50, 250, -250);
+    rotateOnIT(-50, -50, -250, -250);
+    rotateOnIT(-50, 50, -250, 250);
+
+    rotateOnIT(-40, -40, 40, 40);
 
 
     glFlush();
 }
 
 void init() {
-    int start = -30, end = 30;
+    int start = -300, end = 300;
     glPointSize(11);
     glClearColor(1, 1, 1, 1);
     glClear(GL_COLOR_BUFFER_BIT);
